8a8623fd4f6164c3be9dab0efd85fa07e5a6d39e
tlavette <tlavette@yahoo.com>
2019-06-27 00:10:12 -0400

    cleaned up directory system and renamed correct active files

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # TriviaGame
+Basic Trivial Game for Travel
+User will start the game and will have 20 seconds to answer a set of questions.
 
+## Instructions:
+Launch index.html file and press start. - The file utilizes app.js file.
+User is presented with several multiple choice questions.
 
-<!-- Basic Trivial Game for Travel
-User will start the game and will have 20 seconds to answer a set of questions. -->
+The timer is set to end, and displays a "time is up" alert window.
+
+##Technology Used
+Javascript
+jQuery
+HTML/CSS
+
+## Action Items
+I enjoy making this game for my daughters.  Action Items include:
+- Upgrade styling
+- Add submit button with function to compare values with values in answer array
+- Update score
+- clear screen upon time expired.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,154 +1,70 @@
-
 //start timer on click.
-$("#start").on("click", function () {
+$("#start").on("click", function() {
 
 
     //  Set the button alert's timeout to run thirty seconds after pressed.
-    delayButtonAlert = setTimeout(function () {
-        alert("Time is up!  How did you do?");
+    delayButtonAlert = setTimeout(function() {
+      alert("Time is up!  How did you do?");
     }, 30000);
-});
+  });
 
 
-const quizQuestions = [
-    {
-        //properties are separated by commas
-        question: "What is the capital of Georgia?",
-        choices: ["Alabama", "Los Angeles", "Atlanta", "Toronto"],
-        answer: 2
 
-    },
+  function Question (props) {
+    return (
+      `<h2>${props.question}</h2>
+       <div class="btn-group btn-group-toggle" data-toggle="buttons>
 
+         <div class="form-group">
+          <label>${props.a}</label> 
+         <input type="radio" name1="options" id="option1" value="${props.a}" />
+       </div>
+       <div class="form-group">
+         <label>${props.b}</label> 
+         <input type="radio" name2="options" id="option2" value="${props.b}" />
+       </div>
+       <div class="form-group">
+         <label>${props.c}</label> 
+         <input type="radio" name3="options" id="option3" value="${props.c}" />
+       </div>
+        <div class="form-group">
+         <label>${props.d}</label> 
+         <input type="radio" name4="options" id="option4" value="${props.d}" />
+       </div>`
 
-    // question #2
-    {
-        question: "How many syllables is in the word investigate?",
-        choices: [2, 1, 7, 4],
-        answer: 3
+       );
 
-    },
-
-
-    // question #3
-    {
-        question: "Which sentence is the correct past tense of the verb (go)?",
-        choices: ["Yesterday I went to the store to buy some clothes", "Yesterday I'm going to the store to buy some clothes", "Yesterday I will go to the store to buy some clothes", "Yesterday I should go to the store to buy some clothes"],
-        answer: 0
-    },
-
-    //  question #4
-    {
-        question: "If the cost of bananas are $.49 per pound, how much does it cost for 4.5 pounds of bananas?",
-        choices: ["$2.50", "$3.70", "$2.21", "$8.00"],
-        answer: 2
-    },
-
-    // question #5
-    {
-        question: "What do the stars represent in the flag of the United States?",
-        choices: ["The number of members in The Congress", "The number of States in the United States", "The number of US Presidents", "The year the US won it's independence from England"],
-        answer: 1
+       
     }
 
-];
-
-
-console.log(quizQuestions);
-
-function Question(props) {
-var container = $("<div>");
-var header = $("<h2>");
-header.text(props.question);
-container.append(header);
-
-    
-
-   
-//  { 
-
-//     return (
-//       `<h2>${props.question}</h2>
-//        <div class="form-group">
-//          <label>${props.question}</label> 
-//          <input type="radio" value="${props.question}" />
-//        </div>
-//        <div class="form-group">
-//          <label>${props.b}</label> 
-//          <input type="radio" value="${props.question}" />
-//        </div>
-//        <div class="form-group">
-//          <label>${props.c}</label> 
-//          <input type="radio" value="${props.question}" />
-//        </div>
-//         <div class="form-group">
-//          <label>${props.d}</label> 
-//          <input type="radio" value="${props.question}" />
-//        </div>`
-
-//        );
-// }
-
-function QRow(question) {
-    return (
-        `<div class="row">
+    function QRow (question) {
+        return (
+          `<div class="row">
                 <div class="col">
                   ${Question(question)}
                 </div>
             </div>`
-    );
-}
-
-//Will need questions to render when start button is hit.
-function renderQuestions() {
-    let questionsHtml = "";
-    for (let i = 0; i < quizQuestions.length; i++) {
-        questionsHtml += QRow(quizQuestions[i]);
+        );
     }
-    return questionsHtml;
-}
-
-function QuestionsContainer() {
-    return `<div class="container">
+    
+    //Will need questions to render when start button is hit.
+    function renderQuestions () {
+      let questionsHtml = "";
+      for (let i = 0; i < quizQuestions.length; i++) {
+        questionsHtml += QRow(quizQuestions[i]);
+      }
+      return questionsHtml;
+    }
+    
+    function QuestionsContainer () {
+      return `<div class="container">
         ${renderQuestions()}
       </div>`;
-}
+    }
 
-const newRow = $("#root").html(QuestionsContainer());
-    // const newScore = $("score").h
-
-
-
+    
+    
+    const newRow = $("#root").html( QuestionsContainer() );
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// 
-// Create a test FormData object
-// var formData = new FormData();
-// formData.append('c', 'What is the capital of Georgia?');
-// formData.append('d', 'How many syllables is in the word investigate?');
-
-// // Display the values
-// for (var value of formData.values()) {
-//    console.log(value); 
-
-
-// }
-
-// // Function to render questions on the screen()
-// function renderQuestion() {
-
-
-//     document.querySelector("#quizQuestions").innerHTML
-// }
-// // 
